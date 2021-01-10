bfd29f7aee8c61dea8212b7fb16b3fe2fbe43a8c
Evan Cui <evanycui@gmail.com>
2021-01-10 15:39:47 -0500

    rename helper functions to bin

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
+const API_ROUTE = process.env.API_ROUTE;
 
 // register user
-const register = require('../functions/register');
-router.post('/api/register', register);
+const register = require('../bin/register');
+router.post(`${API_ROUTE}/register`, register);
 
 // authorize user
-const { login, logout } = require('../functions/authorize');
-router.post('/api/login', login);
-router.post('/api/logout', logout);
+const { login, logout } = require('../bin/authorize');
+router.post(`${API_ROUTE}/login`, login);
+router.post(`${API_ROUTE}/logout`, logout);
+
+// play game
+const {} = require('../bin/play');
+router.post(`${API_ROUTE}/play`, play);
 
 module.exports = router;
