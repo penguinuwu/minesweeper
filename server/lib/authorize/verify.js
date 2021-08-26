cd4bb5e39426e5c29a9c66bdf225ee39d12652e9
Evan Cui <yichen.cui@mail.utoronto.ca>
2021-08-26 01:55:23 -0400

    Migrate server code from Javascript to Typescript (#4)
    * rename files from js to ts
    
    * this commit is getting too big
    
    * no build errors
    
    * fix runtime errors
    
    * server works now
    
    * clean up socket code

--- server/lib/authorize/verify.js
+++
@@ -1,8 +0,0 @@
-const verify = (req, res) => {
-  // redirect to home if logged in
-  if (req.isAuthenticated())
-    return res.status(200).send({ username: req.user.username });
-  return res.status(403).send('You are not logged in.');
-};
-
-module.exports = verify;
